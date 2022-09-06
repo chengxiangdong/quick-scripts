ec3a253663f5da26fa7d1eed1c86293a377d50b4
ChengXiangdong <chengxiangdong@outlook.com>
2022-09-06 11:20:19 +0800

    Update install-k8s.sh

--- scripts/k8s/install-k8s.sh
+++ scripts/k8s/install-k8s.sh
@@ -194,13 +194,12 @@ function uninstall_docker() {
 }
 
 function install_docker() {
-  docker_ver = $1
   echoTitle 'Install utils'
   yum -y install yum-utils device-mapper-persistent-data lvm2
 
-  echoTitle 'Install docker '${docker_ver}
+  echoTitle 'Install docker '${docker_version}
   yum-config-manager --add-repo http://download.docker.com/linux/centos/docker-ce.repo
-  yum -y install docker-ce-${docker_ver} docker-ce-cli-${docker_ver} containerd.io
+  yum -y install docker-ce-${docker_version} docker-ce-cli-${docker_version} containerd.io
 
   echoTitle 'Start docker service'
   systemctl stop docker && systemctl start docker && systemctl enable docker
@@ -291,8 +290,8 @@ uninstall_docker
 uninstall_k8s
 
 set -e
-install_docker ${docker_version}
-install_k8s ${kubernetes_version}
+install_docker
+install_k8s
 
 if [ ${node_type} == 'm' ]; then
   init_master
