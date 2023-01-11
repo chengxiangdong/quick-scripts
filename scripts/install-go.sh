8432e8ea80fda29bcf4de1ffc2d6dbcf2d7a3fd7
ChengXiangdong <chengxiangdong@outlook.com>
2023-01-11 17:08:40 +0800

    Update install-go.sh

--- scripts/install-go.sh
+++ scripts/install-go.sh
@@ -2,7 +2,7 @@
 # https://raw.githubusercontent.com/chengxiangdong/quick-scripts/main/scripts/install-go.sh
 
 yum install -y wget
-wget https://dl.google.com/go/go1.18.4.linux-amd64.tar.gz
+wget https://dl.google.com/go/go1.19.4.linux-amd64.tar.gz
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.19.4.linux-amd64.tar.gz
 
 echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.bash_profile
